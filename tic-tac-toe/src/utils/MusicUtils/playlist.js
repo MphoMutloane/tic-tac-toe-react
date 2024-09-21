a500abb85de43eb06e5d2786dcf27a9ef5bd35fc
Mpho <mphomutloane.mm@gmail.com>
2024-09-21 07:58:12 +0200

    added playlist

---
+++ tic-tac-toe/src/utils/MusicUtils/playlist.js
@@ -0,0 +1,16 @@
+const playList = [
+    "https://jetsetradio.live/radio/stations/lofi/90sFlav - Balmy.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/90sFlav - Call Me.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Adam Kay - Rehab (Instrumental).mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Afternoon - spring (w park bird).mp3",
+    "https://jetsetradio.live/radio/stations/lofi/App - My First.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Aso - Coffee.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Burbank - Moonlights.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Burbank - A Beautiful Name.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Zeeky Beats - Missed Calls.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Zeeky Beats - Fresh Air.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Zeeky Beats - Gone For Now.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/GentleBeatz - Silent Thoughts.mp3"
+]
+
+export default playList;
